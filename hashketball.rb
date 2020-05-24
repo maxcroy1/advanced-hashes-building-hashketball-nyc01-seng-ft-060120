4d1c98702aff6d275c47d4e25f907cee03cb981d
Maxwell Croy <max.croy1@gmail.com>
2020-05-24 13:45:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -16,14 +16,14 @@ def game_hash
         :slam_dunks => 1
       }, {
         :player_name => "Reggie Evans",
-        :number => ,
-        :shoe => ,
-        :points => ,
-        :rebounds => ,
-        :assists => ,
-        :steals => ,
-        :blocks => ,
-        :slam_dunks =>
+        :number => 30,
+        :shoe => 14,
+        :points => 12,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 12,
+        :blocks => 12,
+        :slam_dunks => 7
       }, {
         :player_name => ,
         :number => ,
