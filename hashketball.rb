64b19c42a99e0e1de0d4f4346a08bcdf175e9831
Maxwell Croy <max.croy1@gmail.com>
2020-05-24 13:42:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,9 +2,19 @@
 def game_hash
   hash = {
     :home =>{
-      :team_name = ,
-      :colors = [],
-      
+      :team_name => ,
+      :colors => [],
+      :players => [{
+        :player_name => ,
+        :number => ,
+        :shoe => ,
+        :points => ,
+        :rebounds => ,
+        :assists => ,
+        :steals => ,
+        :blocks => ,
+        :slam_dunks
+      }]
     },
     :away =>{}
   }
